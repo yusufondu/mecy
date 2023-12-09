bdbc1dae73c39763902976246fd22e48c53028ef
Yusuf Öndü <yusuf.ondu@pusulayazilim.com.tr>
2023-12-09 19:56:26 +0300

    feat: added mix fixes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// Tarayıcı yenilendiğinde çağrılacak fonksiyon
+function restoreScrollPosition() {
+  // Eğer tarayıcı yerel depolama özelliğini destekliyorsa
+  if (typeof Storage !== "undefined") {
+    // Kaydedilmiş scroll pozisyonunu al
+    var scrollPosition = localStorage.getItem("scrollPosition");
+
+    // Eğer scroll pozisyonu varsa, sayfayı bu konuma kaydır
+    if (scrollPosition) {
+      window.scrollTo(0, scrollPosition);
+    }
+  }
+}
+
+// Sayfa yüklenirken scroll pozisyonunu kaydet
+window.addEventListener("beforeunload", function () {
+  // Eğer tarayıcı yerel depolama özelliğini destekliyorsa
+  if (typeof Storage !== "undefined") {
+    // Mevcut scroll pozisyonunu al ve yerel depolamaya kaydet
+    localStorage.setItem("scrollPosition", window.scrollY);
+  }
+});
+
+// Sayfa yüklendiğinde scroll pozisyonunu geri yükle
+window.addEventListener("load", function () {
+  restoreScrollPosition();
+});
+
 const multipleItemCarousel = document.querySelector("#carouselExample");
 
 if (window.matchMedia("(min-width: 576px)").matches) {
@@ -74,4 +102,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Button'a tıklandığında scrollToSection fonksiyonunu çağır
   scrollToFormFromHeader.addEventListener("click", scrollToSection);
+});
+
+//////////////
+
+document.addEventListener("DOMContentLoaded", function () {
+  var openModalButton = document.getElementById("openCookieModal");
+  var modal = document.getElementById("cookieModal");
+  var body = document.body;
+
+  openModalButton.addEventListener("click", function () {
+    modal.style.display = "flex";
+    body.style.overflow = "hidden"; // Kaydırmayı engelle
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      body.style.overflow = "auto"; // Kaydırmayı tekrar etkinleştir
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var openModalButton = document.getElementById("openPrivacyModal");
+  var modal = document.getElementById("privacyModal");
+  var body = document.body;
+
+  openModalButton.addEventListener("click", function () {
+    modal.style.display = "flex";
+    body.style.overflow = "hidden"; // Kaydırmayı engelle
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      body.style.overflow = "auto"; // Kaydırmayı tekrar etkinleştir
+    }
+  });
 });
